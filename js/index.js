a1e44afcea8f10377d2e5a64a4a722329bd54754
Isaabomohana <mrissaam@outlook.com>
2017-06-26 11:38:32 +0200

    fix side-nav closing

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-  $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav({
+     closeOnClick: true,
+     draggable: true
+  });
   $('.parallax').parallax();
   $('body').removeClass('fade-out');
   $('.scrollspy').scrollSpy();
