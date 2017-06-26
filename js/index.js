6e6929a3b09f212127bd1716ae9a09481f82ff21
Isaabomohana <mrissaam@outlook.com>
2017-06-26 11:09:10 +0200

    my Temp

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+  $('.parallax').parallax();
+  $('body').removeClass('fade-out');
+  $('.scrollspy').scrollSpy();
+
+  jQuery(document).ready(function() {
+      jQuery('.post2').addClass("hidden").viewportChecker({
+          classToAdd: 'visible animated fadeInUp', // Class to add to the elements when they are visible
+          offset: 100
+         });
+     jQuery('.post').addClass("hidden").viewportChecker({
+         classToAdd: 'visible animated fadeInDown', // Class to add to the elements when they are visible
+         offset: 100
+        });
+    });
+});
