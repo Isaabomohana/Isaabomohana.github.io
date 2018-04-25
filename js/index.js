035c8727f47fc35fbf40c266770efa30db6d4aeb
Isaabomohana <mrissaam@outlook.com>
2018-04-25 09:31:36 +0200

    update cv

--- js/index.js
+++ js/index.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
   $('.scrollspy').scrollSpy();
 
   jQuery(document).ready(function() {
-      jQuery('.post2').addClass("hidden").viewportChecker({
-          classToAdd: 'visible animated fadeInUp', // Class to add to the elements when they are visible
-          offset: 100
-         });
-     jQuery('.post').addClass("hidden").viewportChecker({
-         classToAdd: 'visible animated fadeInDown', // Class to add to the elements when they are visible
-         offset: 100
-        });
+    jQuery('.post2').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInUp', // Class to add to the elements when they are visible
+      offset: 100
     });
+    jQuery('.post').addClass("hidden").viewportChecker({
+      classToAdd: 'visible animated fadeInDown', // Class to add to the elements when they are visible
+      offset: 100
+    });
+  });
 });
